9d84136942ed99215dc8919537fa53eb3e060661
dexstar1 <jonsaix852@gmail.com>
2020-03-26 18:13:32 +0100

    access origin allowed

--- php-react/delete-user.php
+++ php-react/delete-user.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: https://mycity.com");
 
 require 'db_connection.php';
 $data = json_decode(file_get_contents("php://input"));
